c1f91fd85340625aaba3cbbb6d1baed8af33cedf
Pretbot <chuquilinsegundo0@gmail.com>
2026-04-17 13:04:45 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-//const { WebcastPushConnection } = require('tiktok-live-connector');
-//const express = require('express');
-//const http = require('http');
-//const { Server } = require('socket.io');
-//const cors = require('cors');
-//const path = require('path');
+const { WebcastPushConnection } = require('tiktok-live-connector');
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+const path = require('path');
 
 // ─── Logger ───────────────────────────────────────────────────────────────────
 const log = {
